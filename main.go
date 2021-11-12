86db9bc017a98a516507a074dc05aaf4e49ce269
Sachin Lendis <you@example.com>
2021-11-12 13:06:28 +0200

    init commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(mysql.Open("root:@GetKomfy23:$@/fuudi_admin"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	fmt.Println(db)
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8000")
+}
